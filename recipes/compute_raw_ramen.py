0a7c890c838d99a2d97ed5a4b40c36b6d5a6d595
admin <makoto.miyazaki@dataiku.com>
2021-05-31 14:53:35 +0200

    Saved recipe: RAMENMEITEN.compute_raw_ramen

--- recipes/compute_raw_ramen.py
+++ recipes/compute_raw_ramen.py
@@ -3,10 +3,6 @@ import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 import requests
-from bs4 import BeautifulSoup
-import re
-import pandas as pd
-import time
 from tabelog import Tabelog
 
 raw_ramen_df = Tabelog(base_url="https://tabelog.com/tokyo/rstLst/ramen/", test_mode=True, p_ward='東京都内')
