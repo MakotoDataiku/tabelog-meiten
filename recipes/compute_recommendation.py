896ca49d5438bd3f2882a8138a838433d6133d14
admin <makoto.miyazaki@dataiku.com>
2021-06-04 00:39:41 +0200

    Recipe RAMENMEITEN.compute_recommendation saved through notebook notebook editor for compute_recommendation

--- recipes/compute_recommendation.py
+++ recipes/compute_recommendation.py
@@ -1,24 +1,97 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+from gensim.models import word2vec
+import itertools
+from itertools import product
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+def cos_sim(v1, v2):
+    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
-word2vec = dataiku.Folder("m9JZdV7b")
-word2vec_info = word2vec.get_info()
+model_folder_path = dataiku.Folder("m9JZdV7b").get_path()
+model_path = model_folder_path + "/word2vec_ramen_model.model"
+ramen_model = word2vec.Word2Vec.load(model_path)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 reviews_TF_IDF = dataiku.Dataset("reviews_TF_IDF")
-reviews_TF_IDF_df = reviews_TF_IDF.get_dataframe()
-tripAdvisor_top_TFIDF_words = dataiku.Folder("FXMfF0DU")
-tripAdvisor_top_TFIDF_words_info = tripAdvisor_top_TFIDF_words.get_info()
+df = reviews_TF_IDF.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+tripAdvisor_path = dataiku.Folder("FXMfF0DU").get_path() + "/TripAdvisor_top_TFIDF_words.txt"
+f = open(tripAdvisor_path,'r',encoding="utf-8")
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+words_ta = []
+for i,data in enumerate(f):
+    word = data.replace("'",'').replace('[','').replace(']','').replace(' ','').replace('\n','')
+    words_ta.append(word)
 
-recommendation_df = reviews_TF_IDF_df # For this sample code, simply copy input to output
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+words_ta
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+word_ta_vectors = [ramen_model.wv[w] for w in words_ta]
 
-# Write recipe outputs
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+word_list = []
+arr = df['texts_tfidf_sorted_top20'].values
+for a in arr:
+    l = a.replace("'", "").replace("[", "").replace("]", "").replace(" ", "").split(",")
+    word_list.append(l)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+word_list.append(words_ta)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+uniq_words = list(set(itertools.chain.from_iterable(word_list)))    
+scores = {}
+for word1, word2 in product(uniq_words, repeat=2):
+    # print(word1, word2)
+    scores[(word1, word2)] =  cos_sim(ramen_model.wv[word1], ramen_model.wv[word2])
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+scores
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+avg_avg_scores = []
+for word_2 in words_ta:
+    avg_scores = []
+    for review in df["texts_tfidf_sorted_top20"].values:
+        review = review.replace("'", "").replace("[", "").replace("]", "").replace(" ", "").split(",")
+        word_cross_scores = []
+        for word_1 in review:
+            score = scores[(word_1, word_2)]
+            word_cross_scores.append(score)
+        avg_scores.append(np.mean(word_cross_scores))
+    avg_avg_scores.append(np.mean(avg_scores))
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+avg_avg_scores = []
+for review in df["texts_tfidf_sorted_top20"].values:
+    review = review.replace("'", "").replace("[", "").replace("]", "").replace(" ", "").split(",")
+    avg_scores = []
+    for word_1 in review:
+        word_cross_scores = []
+        for word_2 in words_ta:
+            score = scores[(word_1, word_2)]
+            word_cross_scores.append(score)
+        avg_scores.append(np.mean(word_cross_scores))
+    avg_avg_scores.append(np.mean(avg_scores))
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+len(avg_avg_scores)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['similarity_score'] = avg_avg_scores
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 recommendation = dataiku.Dataset("recommendation")
-recommendation.write_with_schema(recommendation_df)
+recommendation.write_with_schema(df)
