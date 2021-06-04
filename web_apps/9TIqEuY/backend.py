eb174bdb317312456b66f3ff5198d5ab1b7096e3
admin <makoto.miyazaki@dataiku.com>
2021-06-04 18:27:52 +0200

    Saved web app find your best ramen! (RAMENMEITEN.9TIqEuY)

--- web_apps/9TIqEuY/backend.py
+++ web_apps/9TIqEuY/backend.py
@@ -3,6 +3,32 @@ import dataiku
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+from dataiku import pandasutils as pdu
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+PATH = "/Users/mmiyazaki/chromedriver"
+driver = webdriver.Chrome(PATH)
+driver.implicitly_wait(10) # this lets webdriver wait 10 seconds for the website to load
+driver.get("https://tabelog.com/")
+
+inputRestaurant = driver.find_element_by_id("sk")
+inputRestaurant.send_keys('らぁ麺 鳳仙花')
+
+inputArea = driver.find_element_by_id("sa")
+inputArea.send_keys('東京')
+
+driver.find_element_by_id('js-global-search-btn').click()
+
+# driver.find_element_by_class_name('list-rst__rst-name')
+
+list_res = driver.find_elements(By.CLASS_NAME, 'list-rst__rst-name-target')
+
+link_ramen = list_res[0].get_attribute("href")
+
+link_ramen
+
+
 
 # This loads dummy data into a dataframe
 df = pd.DataFrame({
