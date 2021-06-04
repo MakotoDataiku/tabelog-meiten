22837beaa2c1f0359b4dc0b61374b60dd31808ee
admin <makoto.miyazaki@dataiku.com>
2021-06-04 14:35:33 +0200

    Recipe RAMENMEITEN.compute_cluster_visualized saved through notebook notebook editor for compute_cluster_visualized

--- recipes/compute_cluster_visualized.py
+++ recipes/compute_cluster_visualized.py
@@ -1,13 +1,35 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+import matplotlib.pyplot as plt
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 ramen_vocab_clustered = dataiku.Dataset("ramen_vocab_clustered")
-ramen_vocab_clustered_df = ramen_vocab_clustered.get_dataframe()
+df = ramen_vocab_clustered.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.head()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+for cluster in df['cluster_labels'].unique():
+    print(cluster)
+    print(df[df['cluster_labels']==cluster].words.values[:100])
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+labels = {
+    "cluster_0":"立地",
+    "cluster_1":"外観",
+    "cluster_2":"原材料",
+    "cluster_3":"味・風味",
+    "cluster_4":"交通手段",
+    "cluster_5":"営業形態",
+    "cluster_6":"具材",
+}
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
