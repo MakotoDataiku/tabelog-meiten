d414f4e27e360495144cd99f567f9a8e55bcd451
admin <makoto.miyazaki@dataiku.com>
2021-06-04 12:22:23 +0200

    edit files

--- lib/python/clear_stop_words.py
+++ lib/python/clear_stop_words.py
@@ -12,5 +12,5 @@ def clean_text(text):
     text = re.sub(r'\d+', '', text) # 数字の置換。意味的な分析ででは、数字の具体的な値を使えないことが多く、いたずらにボキャブラリを増やすだけで、後のタスクでは役に立たないことが多いため
     text = re.sub(r'[!-/:-@[-`{-~]', r' ', text) # 半角記号の置換
     text = re.sub(u'[■-♯【】]', '', text) # 全角記号の置換 (ここでは0x25A0 - 0x266Fのブロックのみを除去)
-    text = text.replace(u' ', '').replace(u'　', '').replace(u'「', '').replace(u'」', '').replace('・', "").replace('、', "").replace('月', "").replace('日', "")
+    text = text.replace(u' ', '').replace(u'　', '').replace(u'「', '').replace(u'」', '').replace('・', "").replace('、', "").replace('年', "").replace('月', "").replace('日', "")
     return text
