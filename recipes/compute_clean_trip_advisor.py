bf727ac236678fc02a3ff55124529d665acf313f
admin <makoto.miyazaki@dataiku.com>
2021-06-04 13:14:05 +0200

    Saved recipe: RAMENMEITEN.compute_clean_trip_advisor

---
+++ recipes/compute_clean_trip_advisor.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+from clear_stop_words import clean_text
+
+# Read recipe inputs
+raw_ramen = dataiku.Dataset("your_trip_advisor")
+df = raw_ramen.get_dataframe()
+
+df["review"] = df["review"].apply(clean_text)
+
+# Write recipe outputs
+clean_ramen = dataiku.Dataset("clean_trip_advisor")
+clean_ramen.write_with_schema(df)
