42ce2eabb55b79bcee28c1ba5bffd622da2efaa2
admin <makoto.miyazaki@dataiku.com>
2021-06-06 12:24:20 +0200

    Saved web app Play with the ramen model! (RAMENMEITEN.h5KHtUj)

--- web_apps/h5KHtUj/backend.py
+++ web_apps/h5KHtUj/backend.py
@@ -89,8 +89,8 @@ def update_ramen_output(n_clicks, value):
             y = list(w)
             print(y)
             # y[1] = round(y[1], 4)
-            y[1] = translator.translate(y[0], dest='en').text
-            print(translator.translate(y[0], dest='en').text)
+            y[1] = translator.translate(y[0], , src='ja', dest='en').text
+            print(translator.translate(y[0], , src='ja', dest='en').text)
             w = tuple(y)
             #print(w)
             textarea.append(str(w))
@@ -109,7 +109,7 @@ def update_wiki_output(n_clicks, value):
         for w in similar_words:
             y = list(w)
             # y[1] = round(y[1], 4)
-            y[1] = translator.translate(y[0], dest='en').text
+            y[1] = translator.translate(y[0], src='ja', dest='en').text
             w = tuple(y)
             textarea.append(str(w))
             textarea.append(html.Br())      
