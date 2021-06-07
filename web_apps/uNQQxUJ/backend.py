170594da31e6d5c7f37b4b7cd202ea45bbf75ec6
admin <makoto.miyazaki@dataiku.com>
2021-06-07 17:27:58 +0200

    Saved web app Vizualising ramen word map ver2 (RAMENMEITEN.uNQQxUJ)

--- web_apps/uNQQxUJ/backend.py
+++ web_apps/uNQQxUJ/backend.py
@@ -31,7 +31,7 @@ titlecolor = 'rgb(230, 230, 0)'
 
 # others
 # translator = Translator(service_urls=['translate.googleapis.com'])
-translator = GoogleTranslator(source='en', target='jp')  # output -> Weiter so, du bist großartig
+translator = GoogleTranslator(source='en', target='ja')  # output -> Weiter so, du bist großartig
 
 
 # Components
