8ee7117db4134ef425f616c4b61d4dfc0d9b66ba
admin <makoto.miyazaki@dataiku.com>
2021-06-07 11:53:18 +0200

    Saved web app Vizualising ramen word map (RAMENMEITEN.KVjn7bA)

--- web_apps/KVjn7bA/backend.py
+++ web_apps/KVjn7bA/backend.py
@@ -44,7 +44,7 @@ fig.update_layout(
 app.layout = html.Div(children=[
     html.H1(
         children='Vizualizing the ramen universe',
-        style={'backgroundColor':'black'}),
+        style={'backgroundColor':'white'}),
 
     dcc.Graph(
         id='example-graph',
