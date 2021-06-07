8ce7757eb938452014527e94d6aff74d4c338f8b
admin <makoto.miyazaki@dataiku.com>
2021-06-07 11:42:55 +0200

    Saved web app Vizualising ramen word map (RAMENMEITEN.KVjn7bA)

--- web_apps/KVjn7bA/backend.py
+++ web_apps/KVjn7bA/backend.py
@@ -14,7 +14,7 @@ df = dataset.get_dataframe()
 
 fig = px.scatter_3d(df, x='x', y='y', z='z', 
                     opacity=0.4, 
-                    color='cluster_labels'
+                    color='cluster_labels',
                     hover_data=["words"])
 
 fig.update_layout(plot_bgcolor='black',
