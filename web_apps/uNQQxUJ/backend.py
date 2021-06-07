799a76a111e411242788fbdf1605f02d4b4d885a
admin <makoto.miyazaki@dataiku.com>
2021-06-07 17:14:11 +0200

    Saved web app Vizualising ramen word map ver2 (RAMENMEITEN.uNQQxUJ)

--- web_apps/uNQQxUJ/backend.py
+++ web_apps/uNQQxUJ/backend.py
@@ -197,6 +197,7 @@ def update_wiki_output(n_clicks, value):
             style = {'color':'white'})
         return div
 
+"""
 @app.callback(
     Output('3d-plot', 'figure'),
     Input('word-button', 'n_clicks'),
@@ -205,4 +206,18 @@ def update_wiki_output(n_clicks, value):
 def update_plot(n_clicks, value):
     if n_clicks > 0:
         similar_words = ramen_model.wv.most_similar(value)
+                for w in similar_words:
+            word = w[0]
+            df_sliced = df[df['words']==word]
+            x = df_sliced.x.values[0]
+            y = df_sliced.y.values[0]
+            x = df_sliced.z.values[0]
+            print(x, y, z)
+            """
+            fig.add_scatter(
+                x=x, y= y, z=z, 
+                mode="markers",
+                marker=dict(size=20, color="MediumPurple"))
+            """
         return html.Div()
+"""
