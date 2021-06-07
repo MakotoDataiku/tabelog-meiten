5a9b71c2765182262d956e803f6b14aedc25c46e
admin <makoto.miyazaki@dataiku.com>
2021-06-07 23:51:43 +0200

    Saved web app Vizualising ramen word map ver2 (RAMENMEITEN.uNQQxUJ)

--- web_apps/uNQQxUJ/backend.py
+++ web_apps/uNQQxUJ/backend.py
@@ -107,7 +107,7 @@ fig.update_traces(marker=dict(
                   # selector=dict(mode='markers')
                  )
 
-fig.add_traces(
+fig.add_trace(
     x=[0], y=[0], z=[0], marker=dict(size=10), overwrite=True
     
 )
