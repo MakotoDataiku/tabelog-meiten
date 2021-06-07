a63d13af81b28da96db959ace662acf91567d9b5
admin <makoto.miyazaki@dataiku.com>
2021-06-07 16:55:07 +0200

    Saved web app Vizualising ramen word map ver2 (RAMENMEITEN.uNQQxUJ)

--- web_apps/uNQQxUJ/backend.py
+++ web_apps/uNQQxUJ/backend.py
@@ -131,7 +131,7 @@ app.layout = html.Div(
                         'backgroundColor':'black', 
                         'width': '80%',
                         'display': 'inline-block',
-                        'vertical-align': 'top'
+                        'vertical-align': 'middle'
                     }),
                 html.Div(
                     children=[
