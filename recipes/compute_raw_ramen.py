33757b12cd9956ffd57b305c5eb075bca3533f42
admin <makoto.miyazaki@dataiku.com>
2021-06-11 20:37:44 +0200

    Saved recipe: RAMENMEITEN.compute_raw_ramen

--- recipes/compute_raw_ramen.py
+++ recipes/compute_raw_ramen.py
@@ -1,4 +1,3 @@
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -6,7 +5,9 @@ from dataiku import pandasutils as pdu
 from tabelog import Tabelog
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-raw_ramen = Tabelog(base_url="https://tabelog.com/tokyo/rstLst/ramen/", test_mode=False, p_ward='東京都内', begin_page=31, end_page=40) # begin_page=31, end_page=40 5/6/2021
+raw_ramen = Tabelog(base_url="https://tabelog.com/tokyo/rstLst/ramen/", 
+                    test_mode=False, p_ward='東京都内', 
+                    begin_page=31, end_page=40) # begin_page=31, end_page=40 5/6/2021
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 raw_ramen_df = raw_ramen.df
